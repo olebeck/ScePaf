2897ed38edcbcd93a680526d7e76cddf493ae9a2
olebeck <31539311+olebeck@users.noreply.github.com>
2025-07-04 01:34:37 +0200

    fix dequote

--- emd2yml.py
+++ emd2yml.py
@@ -29,7 +29,7 @@ def loadEmd(filename: str):
         sp = line.split(" ")
         for i in range(0,len(sp), 2):
             key = sp[i].replace(":", "")
-            value = sp[i+1]
+            value = sp[i+1].replace("\"", "")
             if key == "//":
                 break
             match key:
@@ -43,7 +43,7 @@ def loadEmd(filename: str):
                     ent = [value, ""]
                     library.variables.append(ent)
                 case "nidvalue":
-                    ent[1] = value.replace("\"", "")
+                    ent[1] = value
                 case "nidsuffix":
                     library.nidsuffix = value
                 case "version":
@@ -59,18 +59,20 @@ def loadEmd(filename: str):
 def nid(name: str, suffix: str):
     return "0x"+binascii.b2a_hex(bytes(reversed(hashlib.sha1((name+suffix).encode()).digest()[:4]))).decode().upper()
 
+assert nid("_Znwj", "PAF") == "0xFC5CD359"
+
 def main():
     outyml = sys.argv[1]
     emd_filename = sys.argv[2]
-    emds = loadEmd(emd_filename)
+    libraries = loadEmd(emd_filename)
 
     with open(outyml, "w") as f:
-        def out(*args):    
+        def out(*args):
             print(*args, file=f)
         out("version: 2")
         out("firmware: 3.60")
         out("modules:")
-        for library in emds:
+        for library in libraries:
             out(f"  {library.name}:")
             out(f"    libraries:")
             out(f"      {library.name}:")
